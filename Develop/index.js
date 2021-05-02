41ef9f2aaee031a91e6ec057d8fc982d5ba59fd8
madsterest <odea.madeleine@gmail.com>
2021-05-02 15:59:10 +1000

    Added some more questions to the inquirer prompt. Added to the License functions so a badge and link are created from a list of choices

--- Develop/index.js
+++ Develop/index.js
@@ -6,11 +6,18 @@ const inquirer = require("inquirer");
 // TODO: Create an array of questions for user input
 const questions = [
   { name: "title", message: "What is the title of your project?" },
+  { name: "description", message: "Give a brief description of your project" },
   { name: "motivation", message: "What was your motivation for development?" },
   { name: "function", message: "What tasks does your program accomplish?" },
+  {
+    name: "features",
+    message: "What are the main features of your application?",
+  },
   { name: "packages", message: "What programs does the program need to run?" },
   { name: "collaborators", message: "Who collaborated on the project?" },
-  { name: "badge", message: "What is your license badge?" },
+  { name: "badge", message: "What license would you like to use?" },
+  { name: "username", message: "What is your GitHub username?" },
+  { name: "repo", message: "What is the name of your repository?" },
 ];
 
 // TODO: Create a function to write README file
@@ -28,7 +35,7 @@ function init() {
     .then((response) => {
       let render = generateMarkdown(response);
       console.log(render);
-      //   writeToFile(`${response.data}.md`, render)
+      //   writeToFile(`README.md`, render)
     })
     .catch((err) => console.log(err));
 }
